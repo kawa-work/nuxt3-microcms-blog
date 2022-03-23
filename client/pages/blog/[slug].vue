5ae1b6ba1c933be3aaaea6f33391ad024598d3a6
kawa-work <k.kawakami.work@gmail.com>
2022-03-24 02:21:06 +0900

    feat: display article detail

--- client/pages/blog/[slug].vue
+++ client/pages/blog/[slug].vue
@@ -16,9 +16,18 @@ const { data: article } = await useFetch(`/blog/${slug}`, {
     <v-row>
       <v-col md="8">
         <h1 class="title">{{ article.title }}</h1>
+        <v-chip-group>
+          <v-chip v-for="tag in article.tag" :key="tag">
+            {{ tag }}
+          </v-chip>
+        </v-chip-group>
         <p class="publishedAt">
-          <time :datetime="article.publishedAt" v-text="article.publishedAt"></time>
+          公開日: <time :datetime="article.publishedAt" v-text="article.publishedAt"></time>
         </p>
+        <p class="publishedAt">
+          最終更新日: <time :datetime="article.publishedAt" v-text="article.revisedAt"></time>
+        </p>
+        <v-img :src="article.thumbnail.url"></v-img>
         <!-- eslint-disable-next-line vue/no-v-html -->
         <div class="post" v-html="article.body"></div>
       </v-col>
