2962c260874738fe32be49d41bb248363732a21a
kawa-work <k.kawakami.work@gmail.com>
2022-03-29 00:48:28 +0900

    fix: adjust header layout

--- client/components/TheHeader.vue
+++ client/components/TheHeader.vue
@@ -5,13 +5,13 @@ import LinkList from './LinkList.vue'
 <template>
   <v-app-bar color="grey-lighten-2">
     <v-row justify="space-between">
-      <v-col cols="4" align-self="center">
+      <v-col cols="8" align-self="center">
         <v-app-bar-title
           tag="div"
           text="Nuxt3 + microCMS blog template"
         ></v-app-bar-title>
       </v-col>
-      <v-col cols="2" class="d-flex justify-end">
+      <v-col cols="4" class="d-flex justify-end">
         <LinkList></LinkList>
       </v-col>
     </v-row>
