ee6d828563fda228e78505b1d8f533311ff43236
kawa-work <k.kawakami.work@gmail.com>
2022-04-01 23:12:57 +0900

    feat: design sidebar

--- client/components/TheSidebar.vue
+++ client/components/TheSidebar.vue
@@ -1,14 +1,29 @@
 <template>
   <v-navigation-drawer app>
-    <v-list-item>
-      <v-list-item-title class="text-h6"> Index </v-list-item-title>
-    </v-list-item>
-
-    <v-divider></v-divider>
-
-    <v-list dense nav>
+    <v-list class="d-flex flex-column align-center py-12">
       <v-list-item>
-        <v-list-item-title>Header1</v-list-item-title>
+        <v-list-item-avatar>
+          <v-img src="/icon.png" width="32" height="32"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-content class="text-center">
+          <v-list-item-title class="text-h6">kawa-work</v-list-item-title>
+          <v-list-item-subtitle>Nuxt3練習中</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-list nav>
+      <v-list-item nuxt to="/">
+        <v-list-item-title>HOME</v-list-item-title>
+      </v-list-item>
+      <v-list-item nuxt to="/about">
+        <v-list-item-title>ABOUT</v-list-item-title>
+      </v-list-item>
+      <v-list-item nuxt to="/blog">
+        <v-list-item-title>BLOG</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
